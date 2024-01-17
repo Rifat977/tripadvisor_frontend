dbd59bbc77a7a7b6b7c1872060582a952816c3e8
Simanta Roy <roy35-909@diu.edu.bd>
2024-01-17 19:05:22 +0600

    imeage on blog

--- blog/models.py
+++ blog/models.py
@@ -15,6 +15,7 @@ class Blog(models.Model):
     user = models.ForeignKey(User, on_delete = models.CASCADE)
     title = models.CharField(max_length=255)
     description = models.TextField(max_length = 700)
+    imeage = models.ImageField(upload_to="BlogImeage/", null=True, blank=True)
 
 
     def __str__(self) -> str:
